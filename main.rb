c740486029930bf620cb54e5927bfbddae5adb1c
chankane <brawnychocolate@gmail.com>
2019-04-06 23:25:56 +0900

    Success

--- main.rb
+++ main.rb
@@ -1,14 +1,32 @@
-$FNAME = "./data.txt"
+# すべてのパターンを試しているだけ
+# id は 1-12 ではなく 0-11
 
 def read()
-  data = []
-  File.open($FNAME, "r") do |f|
-    while line = f.gets
-      data.append([line[0].to_i, line[2].to_i])
-    end
-  end
+  data = readlines.map { |line| line.split.map(&:to_i) }
   [data[0][0], data[0][1], data[1..-1]]
 end
 
+def cre_powerset(set)
+  (0..set.size).flat_map { |i| set.combination(i).to_a }
+end
 
-p read
+def is_habatsu(members)
+  members.combination(2) { |e1, e2| return false if !$rel[e1][e2] }
+  true
+end
+
+$ans = 0
+
+# ただ読み込み
+$n, $m, _ = *read
+
+# 行列に変換
+$rel = Array.new($n).map { Array.new($n, false) }
+_.each { |e| $rel[e[0] - 1][e[1] - 1] = true }
+
+# すべての組み合わせを取得
+powerset = cre_powerset((0...$n).to_a)
+
+powerset.each { |e| $ans = e.length if e.length > $ans if is_habatsu(e) }
+
+p $ans
--- test.rb
+++ test.rb
@@ -1,5 +1,7 @@
-def hoge()
-  [1, 3]
-end
-
-p hoge
+$n, $m = *gets.split.map(&:to_i)
+$rel = Array.new($n).map { Array.new($n, false) }
+m.times {
+  *gets.split.map(&:to_i)
+  $rel[?][?] = true
+}
+#h = gets
